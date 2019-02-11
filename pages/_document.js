b37e974e856bf095f7a6f26ab64b8f7e14eb5cc2
Jonny Dalgleish <jonny@30acres.com.au>
2019-02-11 16:49:40 -0500

    Upgrades!

--- pages/_document.js
+++ pages/_document.js
@@ -24,14 +24,12 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-        <!-- Google Tag Manager -->
 <script dangerouslySetInnerHTML={{
   __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MJWD2S');`}}></script>
-<!-- End Google Tag Manager -->
           <meta content="Pure Yoga Ottawa" name="author" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
